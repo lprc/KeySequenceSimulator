fd5df26efa7af96b10ed85700431cee42ba27d4f
Simon <patrick2_drd2@web.de>
2020-09-07 18:02:58 +0200

    Projektdateien hinzufügen.

---
+++ KeySequenceSimulator/Group.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace KeySequenceSimulator
+{
+    public class Group : UserControl
+    {
+        public Group()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ KeySequenceSimulator/Sequence.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace KeySequenceSimulator
+{
+    public class Sequence : UserControl
+    {
+        public Sequence()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
