1f80538d00a4ee0a9b7c92241258207d6252a15a
Simon <patrick2_drd2@web.de>
2020-09-10 16:37:58 +0200

    AddButton + Styling + work on Sequence

---
+++ KeySequenceSimulator/AddButton.axaml.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
+using System;
+
+namespace KeySequenceSimulator
+{
+    public class AddButton : Button, IStyleable
+    {
+        public AddButton()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        Type IStyleable.StyleKey => typeof(Button);
+    }
+}
