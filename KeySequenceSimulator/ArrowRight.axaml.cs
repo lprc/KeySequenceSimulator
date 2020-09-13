22a43658cdf335494957cc98ca2f779867ba9372
Simon <patrick2_drd2@web.de>
2020-09-13 14:23:09 +0200

    add action element to sequence

---
+++ KeySequenceSimulator/ArrowRight.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace KeySequenceSimulator
+{
+    public class ArrowRight : UserControl
+    {
+        public ArrowRight()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
--- KeySequenceSimulator/Sequence.axaml.cs
+++ KeySequenceSimulator/Sequence.axaml.cs
@@ -1,12 +1,18 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using System.Collections.Generic;
 
 namespace KeySequenceSimulator
 {
     public class Sequence : UserControl
     {
         private Button btnSequenceNumber;
+        private Panel sequencePanel;
+
+        private List<ActionView> actions = new List<ActionView>();
+
         public Sequence()
         {
             this.InitializeComponent();
@@ -16,11 +22,26 @@ namespace KeySequenceSimulator
         {
             AvaloniaXamlLoader.Load(this);
             btnSequenceNumber = this.FindControl<Button>("btnSequenceNumber");
+            sequencePanel = this.FindControl<Panel>("sequencePanel");
         }
 
         public void SetSequenceButtonNumber(int number)
         {
             btnSequenceNumber.Content = System.Convert.ToString(number);
         }
+
+        public void AddAction(object sender, RoutedEventArgs e)
+        {
+            // Add sequence on Click
+            ActionView action = new ActionView();
+            action.SetValue(DockPanel.DockProperty, Dock.Right);
+            actions.Add(action);
+            //action.SetSequenceButtonNumber(actions.Count);
+            ArrowRight arr = new ArrowRight();
+            arr.SetValue(DockPanel.DockProperty, Dock.Right);
+            sequencePanel.Children.Insert(sequencePanel.Children.Count - 1, arr);
+            sequencePanel.Children.Insert(sequencePanel.Children.Count - 1, action);
+        }
+
     }
 }
