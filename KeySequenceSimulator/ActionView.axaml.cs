8566884b4918b382b4ec454650890ae4d08c2c65
Simon <patrick2_drd2@web.de>
2020-09-15 12:19:07 +0200

    mousebutton and repeat support in action

--- KeySequenceSimulator/ActionView.axaml.cs
+++ KeySequenceSimulator/ActionView.axaml.cs
@@ -9,19 +9,19 @@ namespace KeySequenceSimulator
 {
     public class ActionView : UserControl
     {
+        public Sequence ParentSequence { get; set; }
+
         private ComboBox actionCombobox;
         private Panel part2;
         private Border part3;
 
         private Button keyButton;
         private TextBox sleepText;
-        private Button mouseKeyButton;
+        private ComboBox cbMouseKey;
         private TextBox textText;
 
         private Key key;
-        private MouseButton mouseKey;
-        private EventHandler<KeyEventArgs> changeKeyListener;
-        private EventHandler<PointerReleasedEventArgs> changeMouseKeyListener;
+        private MouseButton mouseButton;
 
         public ActionView()
         {
@@ -38,33 +38,12 @@ namespace KeySequenceSimulator
 
             keyButton = this.FindControl<Button>("btnKey");
             sleepText = this.FindControl<TextBox>("txtSleep");
-            mouseKeyButton = this.FindControl<Button>("btnMouse");
+            cbMouseKey = this.FindControl<ComboBox>("cbMouse");
             textText = this.FindControl<TextBox>("txtText");
 
-            // add listener to combobox
+            // add listeners to comboboxes
             actionCombobox.SelectionChanged += OnSelectionChanged;
-
-            // handler for changing input key
-            changeKeyListener = (sender, e) =>
-            {
-                key = e.Key;
-
-                // remove listener
-                keyButton.KeyUp -= changeKeyListener;
-                keyButton.Content = key.ToString();
-            };
-
-            // handler for changing input mouse key
-            changeMouseKeyListener = (sender, e) =>
-            {
-                //TODO
-                //mouseKey = e.MouseButton;
-
-                // remove listener
-                keyButton.KeyUp -= changeKeyListener;
-                //keyButton.Content = key.ToString();
-                keyButton.Content = "test";
-            };
+            cbMouseKey.SelectionChanged += OnMouseSelectionChanged;
         }
 
         public void OnSelectionChanged(object sender, RoutedEventArgs e)
@@ -72,8 +51,10 @@ namespace KeySequenceSimulator
             // first hide all inputs
             keyButton.SetValue(IsVisibleProperty, false);
             sleepText.SetValue(IsVisibleProperty, false);
-            mouseKeyButton.SetValue(IsVisibleProperty, false);
+            cbMouseKey.SetValue(IsVisibleProperty, false);
             textText.SetValue(IsVisibleProperty, false);
+            part2.SetValue(IsVisibleProperty, true);
+            ParentSequence.SetRepeatSequence(false);
 
             // displays action parts depending on action type
             switch (actionCombobox.SelectedIndex)
@@ -90,10 +71,34 @@ namespace KeySequenceSimulator
                 case 5:
                 case 6:
                 case 7:
-                    mouseKeyButton.SetValue(IsVisibleProperty, true);
+                    cbMouseKey.SetValue(IsVisibleProperty, true);
                     break;
                 case 8:
                     textText.SetValue(IsVisibleProperty, true);
+                    break;
+                case 9: // repeat
+                    part2.SetValue(IsVisibleProperty, false);
+                    ParentSequence.SetRepeatSequence(true);
+                    break;
+            }
+        }
+
+        public void OnMouseSelectionChanged(object sender, RoutedEventArgs e)
+        {
+            // save mouse button
+            switch (actionCombobox.SelectedIndex)
+            {
+                case 0:
+                    mouseButton = MouseButton.Left;
+                    break;
+                case 1:
+                    mouseButton = MouseButton.Right;
+                    break;
+                case 2:
+                    mouseButton = MouseButton.Middle;
+                    break;
+                default:
+                    mouseButton = MouseButton.None;
                     break;
             }
         }
@@ -102,11 +107,18 @@ namespace KeySequenceSimulator
         {
             // Waits for input hotkey
             keyButton.Content = "Waiting for input...";
-            mouseKeyButton.Content = "Waiting for input...";
 
             // add keylistener
             keyButton.KeyUp += changeKeyListener;
-            mouseKeyButton.PointerReleased += changeMouseKeyListener;
+        }
+
+        private void changeKeyListener(object sender, KeyEventArgs e)
+        {
+            key = e.Key;
+
+            // remove listener
+            keyButton.KeyUp -= changeKeyListener;
+            keyButton.Content = key.ToString();
         }
     }
 }
--- KeySequenceSimulator/Sequence.axaml.cs
+++ KeySequenceSimulator/Sequence.axaml.cs
@@ -10,6 +10,7 @@ namespace KeySequenceSimulator
     {
         private Button btnSequenceNumber;
         private Panel sequencePanel;
+        private Button btnAddAction;
 
         private List<ActionView> actions = new List<ActionView>();
 
@@ -23,6 +24,7 @@ namespace KeySequenceSimulator
             AvaloniaXamlLoader.Load(this);
             btnSequenceNumber = this.FindControl<Button>("btnSequenceNumber");
             sequencePanel = this.FindControl<Panel>("sequencePanel");
+            btnAddAction = this.FindControl<Button>("btnAddAction"); 
         }
 
         public void SetSequenceButtonNumber(int number)
@@ -34,6 +36,7 @@ namespace KeySequenceSimulator
         {
             // Add action and arrow on Click
             ActionView action = new ActionView();
+            action.ParentSequence = this;
             action.SetValue(DockPanel.DockProperty, Dock.Right);
             actions.Add(action);
 
@@ -44,5 +47,23 @@ namespace KeySequenceSimulator
             sequencePanel.Children.Insert(sequencePanel.Children.Count - 1, arr);
         }
 
+        // replaces add action button with repeat if repeat is true
+        public void SetRepeatSequence(bool repeat)
+        {
+            btnAddAction.SetValue(IsVisibleProperty, !repeat);
+            if (repeat)
+            {
+                // remove last arrow
+                sequencePanel.Children.RemoveAt(sequencePanel.Children.Count - 2);
+                sequencePanel.Children.RemoveAt(sequencePanel.Children.Count - 2);
+            }
+            else
+            {
+                // add last arrow
+                ArrowRight arr = new ArrowRight();
+                arr.SetValue(DockPanel.DockProperty, Dock.Right);
+                sequencePanel.Children.Insert(sequencePanel.Children.Count - 1, arr);
+            }
+        }
     }
 }
